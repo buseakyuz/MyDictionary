8b8b474a9b7d4538bb3939c11de272bf65d641df
Buse Akyüz <buse.akyuz29@gmail.com>
2021-01-25 00:00:50 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericDictionary
+{
+    class MyGenericDictionary<TKey, TValue>
+    {
+       
+        TKey[] keyArray;
+        TValue[] valueArray;
+       
+        TKey[] tempKeyArray;
+        TValue[] tempValueArray;
+        public MyGenericDictionary()
+        {
+            keyArray = new TKey[0];
+            valueArray = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value) //Ekleme 
+        {
+            // ana array'leri yedekleme işlemi
+            tempKeyArray = keyArray;
+            tempValueArray = valueArray;
+
+            // ana array'lerin her çağırıldıklarında 1'er eleman arttırılması işlemini yapıyoruz.
+            keyArray = new TKey[keyArray.Length + 1];
+            valueArray = new TValue[valueArray.Length + 1];
+
+            // array'in uzunluğu arttırıldıktan sonra geçici array'lerdeki elemanları ana array'lerimize aktarıyoruz.
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keyArray[i] = tempKeyArray[i];
+            }
+
+            for (int i = 0; i < tempValueArray.Length; i++)
+            {
+                valueArray[i] = tempValueArray[i];
+            }
+
+            // array^leri eşitleme işlemi bittikten sonra yeni elemanlarımızı ekliyoruz.
+            keyArray[keyArray.Length - 1] = key;
+            valueArray[valueArray.Length - 1] = value;
+        }
+
+        // iki metot listelerimizi consolda göstermemizi sağlar.
+        public TKey[] Items1
+        {
+            get { return keyArray; }
+        }
+
+        public TValue[] Items2
+        {
+            get { return valueArray; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGenericDictionary<int, string> adSoyadNu = new MyGenericDictionary<int, string>();
+            adSoyadNu.Add(123, "Buse Akyüz");
+            adSoyadNu.Add(345, "Alper Akyüz");
+            adSoyadNu.Add(567, "Büşra Akyüz");
+            adSoyadNu.Add(890, "Engin Demiroğ");
+            adSoyadNu.Add(999, "Demiroğ Engin");
+
+            Console.WriteLine("Öğrenci No: " + adSoyadNu.Items1[0] + " " + "Ad Soyad: " + adSoyadNu.Items2[0]);
+            Console.WriteLine("Öğrenci No: " + adSoyadNu.Items1[1] + " " + "Ad Soyad: " + adSoyadNu.Items2[1]);
+            Console.WriteLine("Öğrenci No: " + adSoyadNu.Items1[2] + " " + "Ad Soyad: " + adSoyadNu.Items2[2]);
+            Console.WriteLine("Öğrenci No: " + adSoyadNu.Items1[3] + " " + "Ad Soyad: " + adSoyadNu.Items2[3]);
+            Console.WriteLine("Öğrenci No: " + adSoyadNu.Items1[4] + " " + "Ad Soyad: " + adSoyadNu.Items2[4]);
+
+        }
+    }
+}
